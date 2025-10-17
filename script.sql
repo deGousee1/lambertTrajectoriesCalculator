cdbcad813d8368f5a6e2ee16bbb1a7d17920a3e6
Michał Ossowski <164339462+deGousee1@users.noreply.github.com>
2025-10-17 18:00:11 +0200

    Added some database dara

---
+++ script.sql
@@ -0,0 +1,22 @@
+create table celestial_bodies
+(
+    id             bigint unsigned auto_increment
+        primary key,
+    name           varchar(255) not null,
+    mu             double       null,
+    radius         double       null,
+    semimajor_axis double       null,
+    eccentricity   double       null,
+    inclination    double       null,
+    raan           double       null,
+    arg_periapsis  double       null,
+    true_anomaly   double       null,
+    soi            double       null,
+    parent_body    text         null,
+    constraint id
+        unique (id),
+    constraint name
+        unique (name)
+);
+
+
